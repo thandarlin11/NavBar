a88b988197bee293652eba3ed52c87496ee441c7
thandarlin11 <thandarlin@easysheets.net>
2022-07-01 16:28:46 +0630

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar  from './components/NavBar/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Navbar/>
+    </div>
+  );
+}
+
+export default App;
